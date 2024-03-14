9f5389383e67e69facc4d87132f130c6b1fbeb65
MartineJA <martine.jamalkhan@3wa.io>
2024-03-14 18:34:32 +0100

    Version école

--- Assets/Script/CollisionAvecLeMonstre.cs
+++ Assets/Script/CollisionAvecLeMonstre.cs
@@ -10,6 +10,12 @@ public class CollisionAvecLeMonstre : MonoBehaviour
     [SerializeField]
     private GameObject m_text;
 
+    [SerializeField]
+    private GameObject m_text02;
+
+    [SerializeField]
+    private ScoreEtoile m_scoreEtoile;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +36,15 @@ public class CollisionAvecLeMonstre : MonoBehaviour
             Debug.Log("Collision entre bonbon et monstre");
             Destroy(m_gameObject);
             m_text.SetActive(true);
+            Time.timeScale = 0f;
+
+        }
+
+        if (collision.CompareTag("Bonbon") && m_scoreEtoile.scoreEtoile <= 0)
+        {
+            Debug.Log("Essaie de gagner l'étoile!");
+            Destroy(m_gameObject);
+            m_text02.SetActive(true);
             Time.timeScale = 0f;
 
         }
--- Assets/Script/CollisionEtoilee.cs
+++ Assets/Script/CollisionEtoilee.cs
@@ -6,14 +6,18 @@ public class CollisionEtoilee : MonoBehaviour
 {
     public ScoreEtoile etoile;
     public GameObject etoileImage;
+    public ParticleSystem etoileSprite;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Bonbon"))
         {
+
+            etoileSprite.Play();
             Destroy(gameObject);
             etoile.scoreEtoile += 1;
             etoileImage.SetActive(true);
+            
         }
 
     }
